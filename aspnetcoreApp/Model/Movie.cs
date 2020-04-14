d8676e9e3424a69f0d156e3acaa22dac0403bb68
lg <532953981@qq.com>
2020-04-14 16:26:36 +0800

    1

---
+++ aspnetcoreApp/Model/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcoreApp.Model
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [StringLength(60,MinimumLength =3)]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name ="release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-z""'\s-]*$")]
+        public string Genre { get; set; }
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public string Rating { get; set; }
+    }
+}
