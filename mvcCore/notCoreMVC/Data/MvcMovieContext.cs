516ff9edd92522e7b8e132f3f1cfc91075787aca
lg <532953981@qq.com>
2020-04-17 09:37:34 +0800

    2

---
+++ mvcCore/notCoreMVC/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using notCoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace notCoreMVC.Data
+{
+    public class MvcMovieContext:DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ mvcCore/notCoreMVC/Migrations/20200416061610_initialcreate3.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace notCoreMVC.Migrations
+{
+    public partial class initialcreate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
